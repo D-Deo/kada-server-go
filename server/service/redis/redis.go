f7ad74ba87897ab4ddcd34f4fd7c388db711f37a
D-Deo <romeo_2111314@hotmail.com>
2020-04-26 11:35:40 +0800

    [add] support redis funcs

--- server/service/redis/redis.go
+++ server/service/redis/redis.go
@@ -2,10 +2,28 @@ package redis
 
 import (
 	"fmt"
-	"log"
+	"kada/server/log"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
+)
+
+const (
+	Min = "-inf"
+	Max = "+inf"
+)
+
+var (
+	String    = redis.String
+	Strings   = redis.Strings
+	StringMap = redis.StringMap
+	Int       = redis.Int
+	Ints      = redis.Ints
+	IntMap    = redis.IntMap
+	Int64     = redis.Int64
+	Int64s    = redis.Int64s
+	Int64Map  = redis.Int64Map
+	Bool      = redis.Bool
 )
 
 type Client struct {
@@ -54,20 +72,42 @@ func (o *Client) Exec(cmd string, key interface{}, args ...interface{}) (interfa
 	return reply, nil
 }
 
-func (o *Client) Get(key string) string {
-	ret, err := redis.String(o.Exec("GET", key))
+func (o *Client) Get(key string, def string) string {
+	ret, err := String(o.Exec("GET", key))
 	if err != nil {
 		log.Printf("[redis] get %v err: %v", key, err)
-		return ""
+		return def
+	}
+	return ret
+}
+
+func (o *Client) HGet(key string, field string, def string) string {
+	ret, err := String(o.Exec("HGET", key, field))
+	if err != nil {
+		log.Printf("[redis] hget %s %s err: %v", key, field, err)
+		return def
 	}
 	return ret
 }
 
 func (o *Client) HGetAll(key string) map[string]string {
-	ret, err := redis.StringMap(o.Exec("HGETALL", key))
+	ret, err := StringMap(o.Exec("HGETALL", key))
 	if err != nil {
 		log.Printf("[redis] hgetall %s err: %v", key, err)
 		return nil
 	}
 	return ret
+}
+
+func (o *Client) ZRANGEBYSCORE(zset string, min string, max string) map[string]string {
+	ret, err := StringMap(o.Exec("ZRANGEBYSCORE", zset, min, max))
+	if err != nil {
+		log.Warn("[redis] zrangebyscore %s %s %s err: %v", zset, min, max, err)
+		return nil
+	}
+	return ret
+}
+
+func (o *Client) NoInclude(num int) string {
+	return fmt.Sprintf("(%d", num)
 }
